6faf70d6ad239bd493562dc664694bfba1efe74c
Ashib Hoque <a.hoque29@hotmail.com>
2023-03-16 03:16:31 +0000

    Added context

---
+++ E-Commerce/E-Commerce.DataAccess/DBContext/Context.cs
@@ -0,0 +1,30 @@
+﻿using E_Commerce.Common.DatabaseModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce.DataAccess.DBContext;
+
+public class Context : DbContext
+{
+	public Context(DbContextOptions<Context> opts) : base(opts)
+	{
+	}
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		modelBuilder.Entity<Order>(e => e.ToTable("Order", "Main"));
+
+		modelBuilder.Entity<Customer>(e => e.ToTable("Customer", "Main"));
+
+		modelBuilder.Entity<Product>(e => e.ToTable("Product", "Main"));
+
+		modelBuilder.Entity<OrderDetail>(e =>
+		{
+			e.ToTable("OrderDetail", "Main");
+			e.HasKey(t => new
+			{
+				t.OrderId,
+				t.ProductId
+			});
+		});
+	}
+}
